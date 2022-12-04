72f4e4b43947d037fbb076d0e045aa73cf9cb9d5
Oluborode Akintunde Paul <64453973+fidelmak@users.noreply.github.com>
2022-12-04 22:21:19 +0100

    new file:   src/utils/Constants.js

---
+++ src/Components/SearchBar.jsx
@@ -0,0 +1,17 @@
+
+import React, {useState} from 'react'
+
+import { useNavigate } from 'react-router-dom'
+import { Paper, IconButton } from '@mui/material'
+import SearchIcon from '@mui/icons-material/Search'
+
+export function SearchBar() {
+    const [searchTerm, setSearchTerm] = useState('');
+    const navigate = useNavigate();
+
+    return (
+        <>
+            
+        </>
+    )
+}
