3cac10782ef58d84c9ab5834ac75b443b1474b8e
Oluborode Akintunde Paul <64453973+fidelmak@users.noreply.github.com>
2022-12-05 23:47:06 +0100

    modified:   src/Components/Feed.jsx

--- src/Components/Feed.jsx
+++ src/Components/Feed.jsx
@@ -1,6 +1,24 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
+import {Box, Stack, Typography} from "@mui/material"
+import {fetchFromAPI} from "../utils/fetchFromAPI"
+import {Videos, Sidebar} from "./"
 
 const Feed = () => {
+
+  const [selectedCategory, setSelectedCategory] = useState("New");
+  const [videos, setVideos] =useState(null)
+
+  useEffect(()=>{
+    setVideos(null);
+
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`).then((data) =>setVideos(data.items))
+  }, [selectedCategory]);
+
+
+
+
+
+
   return (
     <div>Feed</div>
   )
