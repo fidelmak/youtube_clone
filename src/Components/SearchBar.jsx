a0b3f2c6d33231a518dc3f373b13579fe0e85e61
Oluborode Akintunde Paul <64453973+fidelmak@users.noreply.github.com>
2022-12-05 06:06:43 +0100

    modified:   src/Components/SearchBar.jsx 	modified:   src/Components/index.js

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -9,9 +9,46 @@ export function SearchBar() {
     const [searchTerm, setSearchTerm] = useState('');
     const navigate = useNavigate();
 
+    const onHandleSubmit = (e)=>{
+        e.preventDefault();
+
+        if(searchTerm){
+            navigate(`/search/${searchTerm}`
+            );
+            setSearchTerm('');
+        }
+        
+    }
     return (
         <>
+          <Paper
+          components='form'
+          onSubmit={onHandleSubmit}
+          sx={{
+            borderRadius:20,
+            border:'1px solid #e3e3e3',
+            pl:2,
+            boxShadow:'none',
+            mr:{sm:5},
+          }}
+          
+          
+          >
+            <input
+            className='search-bar'
+            placeholder='search...'
+            value={searchTerm}
+            onChange={(e)=>setSearchTerm(e.target)} />
             
+            
+
+                <IconButton type="submit" sx={{p:'10px',color:'red'}} arial-label='search'>
+                    <SearchIcon />
+
+                </IconButton>
+           
+            
+            </Paper>  
         </>
     )
 }
